83ed0cea4ee1f6d98b36014312c63e279fb943d6
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2021-07-08 15:10:47 +0200

    end

--- Relational operators/Check the value/src/Main.java
+++ Relational operators/Check the value/src/Main.java
@@ -4,12 +4,5 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
-        int n = scanner.nextInt();
-
-        if (n > 0 && n < 10) {
-            System.out.println(true);
-        } else {
-            System.out.println(false);
-        }
     }
 }
